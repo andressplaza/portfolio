ce97a8986abaa217e1a5257b7615d48ecd479b80
andressplaza <andvic.1eg1314@gmail.com>
2024-08-17 13:27:02 +0200

    creado menu hamburgesa responsive :)

--- public/app.js
+++ public/app.js
@@ -14,3 +14,14 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+document.querySelector('.hamburger').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+
+    // Alternar entre el ícono de hamburguesa y el de cerrar
+    const barsIcon = this.querySelector('.fa-bars');
+    const timesIcon = this.querySelector('.fa-times');
+    barsIcon.style.display = barsIcon.style.display === 'none' ? 'block' : 'none';
+    timesIcon.style.display = timesIcon.style.display === 'none' ? 'block' : 'none';
+});
